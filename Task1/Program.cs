90bb21f235751358db443c4934a76970e6346ed8
IvanKovalev <deformgame@gmail.com>
2022-10-04 17:49:34 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string [] array = {"hello", "hi", "ola", "bonjour", "buongiorno", "Hej"};
+
+string [] result = new string [array.Length];
+int wordSize = 0;
+
+foreach (string value in array)
+{
+    if (value.Length <=3)
+    {
+    result [wordSize] = value;
+    wordSize++;
+    }
+}
+Console.WriteLine(string.Join(" ", result, 0, wordSize));
